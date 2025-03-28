69cd641e45a1745850026c0a0a033f713e82d9d1
Axel D. <SetTrend@users.noreply.github.com>
2025-03-28 20:36:31 +0100

    Initial commit.

---
+++ XDocumentExplorer/Commands/FindReplaceControl/WrapCommand.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Input;
+
+namespace XDocumentExplorer.Commands.FindReplaceControl
+{
+	public class WrapCommand : RoutedCommand
+	{
+		public WrapCommand() => InputGestures.Add(new KeyGesture(Key.P, ModifierKeys.Control));
+	}
+}
